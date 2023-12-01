af9c3d461421c9b48ba36230bbb4fd145873f5ca
Shrey Batra <shrey.batra@cosmocloud.io>
2023-12-01 12:39:13 +0530

    updated partner logos

--- constants/Clients/index.tsx
+++ constants/Clients/index.tsx
@@ -1,13 +1,13 @@
-import GithubLogo from "@/assets/svg/github.svg";
-import GoogleLogo from "@/assets/svg/google.svg";
+// import GithubLogo from "@/assets/svg/github.svg";
+// import GoogleLogo from "@/assets/svg/google.svg";
 
 export const ClientLogos = [
-  {
-    icon: GoogleLogo,
-    name: "Delcaper",
-  },
-  {
-    icon: GithubLogo,
-    name: "Github",
-  },
+  // {
+  //   icon: GoogleLogo,
+  //   name: "Delcaper",
+  // },
+  // {
+  //   icon: GithubLogo,
+  //   name: "Github",
+  // },
 ];
