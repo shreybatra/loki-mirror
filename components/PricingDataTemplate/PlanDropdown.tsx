ed72f7d98251a8eabcd9ec5bd9c5a5db98255e37
Rishabh Dubey <rishabh.dubey@cosmocloud.io>
2024-02-23 14:50:49 +0530

    fix(priciing): add more padding in select dropdown

--- components/PricingDataTemplate/PlanDropdown.tsx
+++ components/PricingDataTemplate/PlanDropdown.tsx
@@ -23,44 +23,50 @@ const PlanDropdown = ({
     <div className="grid grid-cols-1 md:grid-cols-3 my-[44px] gap-[20px]">
       <div className="plan--dropdown">
         <p className="text-[14px] text-white font-[500]">Cloud</p>
-        <select
-          name="cloud"
-          id="cloud"
-          value={selectedCloud}
-          onChange={(e) => {
-            setSelectedCloud(e.target.value);
-          }}
-        >
-          {cloudTypes.map((val) => {
-            return (
-              <option key={val.key} value={val.key}>
-                {val.displayName}
-              </option>
-            );
-          })}
-        </select>
+        <div className="plan--dropdown--select">
+          <select
+            name="cloud"
+            id="cloud"
+            value={selectedCloud}
+            onChange={(e) => {
+              setSelectedCloud(e.target.value);
+            }}
+          >
+            {cloudTypes.map((val) => {
+              return (
+                <option key={val.key} value={val.key}>
+                  {val.displayName}
+                </option>
+              );
+            })}
+          </select>
+        </div>
       </div>
       <div className="plan--dropdown">
         <p className="text-[14px] text-white font-[500]">Region</p>
-        <select
-          name="region"
-          id="region"
-          value={selectedRegion}
-          onChange={(e) => {
-            setSelectedRegion(e.target.value);
-          }}
-        >
-          {cloudTypes
-            .find((val) => val.key === selectedCloud)
-            ?.regions.map((val) => (
-              <option key={val.key} value={val.key}>
-                {val.displayName} ({val.key})
-              </option>
-            ))}
-        </select>
+        <div className="plan--dropdown--select">
+          <select
+            name="region"
+            id="region"
+            value={selectedRegion}
+            onChange={(e) => {
+              setSelectedRegion(e.target.value);
+            }}
+          >
+            {cloudTypes
+              .find((val) => val.key === selectedCloud)
+              ?.regions.map((val) => (
+                <option key={val.key} value={val.key}>
+                  {val.displayName} ({val.key})
+                </option>
+              ))}
+          </select>
+        </div>
       </div>
       <div className="plan--dropdown">
         <p className="text-[14px] text-white font-[500]">Currency</p>
+        <div className="plan--dropdown--select">
+
         <select
           name="currency"
           id="currency"
@@ -68,13 +74,14 @@ const PlanDropdown = ({
           onChange={(e) => {
             setSelectedCurrency(e.target.value);
           }}
-        >
+          >
           {currencyTypes.map((val) => (
             <option key={val} value={val}>
               {val}
             </option>
           ))}
         </select>
+          </div>
       </div>
     </div>
   );
